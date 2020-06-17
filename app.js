2a008b5e0ccfc642f6995ddb4df1a01d96bfe334
uchchhwash <uchchhwash.git@gmail.com>
2020-06-18 05:41:36 +0600

    get articles complete

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const article = require(__dirname + "/models/articles.js")
+const model = require(__dirname + "/models/articles.js")
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get("/articles", function(req, res) {
+    model.db.connect();
+    let allArticles = [];
+
+    model.Article.find({}, function(err, data) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(data);
+            model.db.close()
+        }
+    })
+
+})
 
 
 app.listen(3000, function() {
--- models/articles.js
+++ models/articles.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 dbConnectionUrl = "mongodb://localhost:27017/wikiDB"
 const db = [];
+
 db.connect = function() {
     mongoose.connect(dbConnectionUrl, { useNewUrlParser: true, useUnifiedTopology: true }, function() {
         //console.log("DB Connected Successfully");
@@ -17,3 +18,21 @@ const articleSchema = {
 }
 
 const Article = mongoose.model("Article", articleSchema);
+
+exports.db = db;
+exports.Article = Article;
+
+// exports.getAllArticle = function() {
+//     db.connect();
+//     let test = [];
+
+//     let foundData = Article.find({}, function(err, data) {
+//         if (err) {
+//             console.log(err);
+//         } else {
+//             test = data;
+//             db.close();
+//         }
+//     })
+//     return test;
+// }
