6ceea7a8162d386ed2e375e24f8f207056cd5f83
uchchhwash <uchchhwash.git@gmail.com>
2020-06-18 04:24:32 +0600

    project init

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.listen(3000, function() {
+    console.log("server is running on http://localhost:3000")
+})
